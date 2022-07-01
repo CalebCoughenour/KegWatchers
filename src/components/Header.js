079eac75ff3e21bd2925a5c88c96c648b584c212
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-07-01 16:08:58 -0700

    add styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,17 @@ import React from "react";
 
 
 function Header() {
+
+  const headerStyles = {
+    textAlign: 'center',
+    textDecoration: 'underline'
+  }
+
   return (
     <React.Fragment>
-      <h1>Keg Watchers</h1>
+      <div style={headerStyles}>
+        <h1>Keg Watchers</h1>
+      </div>
     </React.Fragment>
   );
 }
