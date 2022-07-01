7b31d25491d51987df63fd63ed8e2bd59f68ca52
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-07-01 12:40:37 -0700

    add component to create new keg using reusable form

---
+++ src/components/NewKegForm.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { v4 } from 'uuid';
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
+
+
+function NewKegForm(props) {
+
+  function handleNewKegFormSubmission(event){
+    event.preventDefault();
+    props.onNewKegCreation({name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, abv: event.target.abv.value, description: event.target.description.value, id: v4()});
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleNewKegFormSubmission}
+        buttonText="Submit"
+      />
+    </React.Fragment>
+  );
+}
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
+
+export default NewKegForm;
