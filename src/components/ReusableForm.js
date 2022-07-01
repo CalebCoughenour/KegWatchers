d49a52780f733ca9de05e7d8a0417c1fe36d230f
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-07-01 13:14:41 -0700

    add pints to form

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -8,22 +8,32 @@ function ReusableForm(props) {
         <input
           type='text'
           name='name'
+          required='required'
           placeholder='Beverage Name' />
         <input
           type='text'
           name='brand'
+          required='required'
           placeholder='Brand' />
         <input
           type='text'
           name='price'
+          required='required'
           placeholder='Price of Keg' />
           <input
           type='text'
           name='abv'
+          required='required'
           placeholder='ABV Content' />
           <textarea
           name='description'
+          required='required'
           placeholder='Beverage Description' />
+          <input
+          type='text'
+          name='pints'
+          required='required'
+          placeholder='How Many Pints' />
         <button type='submit'>{props.buttonText}</button>
       </form>
     </React.Fragment>
